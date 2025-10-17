d3c2ca1046d9e6d5a44a6f6828f0f519826b3fcf
devansh <devanshtripathi525@gmail.com>
2025-10-18 00:07:43 +0530

    Log DEBUG and ALLOWED_HOSTS at startup in wsgi/asgi

--- learnify/asgi.py
+++ learnify/asgi.py
@@ -14,3 +14,12 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnify.settings')
 
 application = get_asgi_application()
+
+# Log current DEBUG and ALLOWED_HOSTS so deployment logs show the active settings.
+import logging
+try:
+	from django.conf import settings as _settings
+	logger = logging.getLogger('learnify.startup')
+	logger.info('Django startup - DEBUG=%s, ALLOWED_HOSTS=%s', _settings.DEBUG, _settings.ALLOWED_HOSTS)
+except Exception:
+	logging.getLogger('learnify.startup').exception('Failed to log startup settings')
--- learnify/wsgi.py
+++ learnify/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learnify.settings')
 
 application = get_wsgi_application()
+
+# Log current DEBUG and ALLOWED_HOSTS so deployment logs show the active settings.
+import logging
+try:
+	from django.conf import settings as _settings
+	logger = logging.getLogger('learnify.startup')
+	logger.info('Django startup - DEBUG=%s, ALLOWED_HOSTS=%s', _settings.DEBUG, _settings.ALLOWED_HOSTS)
+except Exception:
+	# If logging fails here, don't crash the WSGI startup; swallow errors.
+	logging.getLogger('learnify.startup').exception('Failed to log startup settings')
